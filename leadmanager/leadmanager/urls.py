e25b0eb9a1ae4b94efbb045c0c940093dcd596ec
Mateo <rosenmatt1@gmail.com>
2020-05-30 15:49:55 -0600

    Fixed the lead urls.py page

--- leadmanager/leadmanager/urls.py
+++ leadmanager/leadmanager/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
+    path('', include('frontend.urls')), #make sure front end loads before back end
     path('', include('leads.urls'))
 ]
