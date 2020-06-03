3279d4969f3652f1986866df374539b889f59bfa
Mateo <rosenmatt1@gmail.com>
2020-06-03 15:36:22 -0600

    Got the app working with useState and use Effect.  Need to build out UI and connect to the backend from the app instead of the preexisting heroku API

--- leadmanager/frontend/src/components/leads/Chuck.js
+++ leadmanager/frontend/src/components/leads/Chuck.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react'
 
 export default function Chuck() {
-
     const [jokes, setJokes] = useState([])
+    const [currentJoke, setCurrentJoke] = useState([])
+    const [tag, setTag] = useState([])
 
     // axios
     // .get("/api/leads/")
@@ -13,28 +14,29 @@ export default function Chuck() {
       }
     
     const generateQuote = () => {
-        const randomIndex = this.getRandomInt(this.state.jokes.length)
-        const randomJoke = this.state.jokes[randomIndex]
-        this.setState({
-          quote: randomJoke.joke,
-          tags: randomJoke.categories
-        })
-        var sound = new Audio("upper-cut.mp3")
-        sound.play()
+        const randomIndex = getRandomInt(jokes.length)
+        console.log("randomIndex", randomIndex)
+        const randomJoke = jokes[randomIndex]
+        console.log("randomJoke", randomJoke)
+      
+        setCurrentJoke(randomJoke.joke)
+        setTag(randomJoke.categories)
+        // var sound = new Audio("upper-cut.mp3")
+        // sound.play()
       }
     
       useEffect(() => {
         fetch("https://chuck-norris-quote-generator.herokuapp.com/jokes")
           .then(data => data.json())
           .then(JSONdata => {
-            console.log(JSONdata)
-            this.setState({ jokes: JSONdata.data.jokes })
+            console.log("Chuck Data", JSONdata)
+            setJokes(JSONdata.data.jokes)
           })
       }, [])
 
     return (
         <div>
-            <h1>Chuck Norris</h1>
+            <h1 onClick={() => generateQuote()}>Chuck Norris</h1>
         </div>
     )
 }
